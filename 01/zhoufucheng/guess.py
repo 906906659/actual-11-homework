6fc09262c994a35053c988226e49cc971b08fa8e
cclist1116 <cclist1116@sina.com>
2016-08-29 16:02:54 +0800

    modify guess.py

--- 01/zhoufucheng/guess.py
+++ 01/zhoufucheng/guess.py
@@ -6,18 +6,18 @@ secret = random.randint(1, 100)
 guess = 0
 tries = 0
 
-print ("唔哈！！ 我是可怕的海盗罗伯茨，我有一个秘密！")
-print ("这是一个从1到99的数字，我只会给你6次机会。")
+print ("\033[34;1m唔哈！！ 我是可怕的海盗罗伯茨，我有一个秘密！\033[0m")
+print ("\033[31;1m这是一个从1到99的数字，我只会给你6次机会。\033[0m")
 
 while guess != secret and tries <6:
-    guess = int(raw_input("你猜这个数是多少?  "))
+    guess = int(raw_input("\033[34;1m你猜这个数是多少?  \033[0m"))
     if guess < secret:
-        print ("太低了，你这只笨狗熊!  ")
+        print ("\033[32;1m太低了，你这只笨狗熊!  \033[0m")
     elif guess > secret:
-        print ("太高了, 你这只笨狗熊!  ")
+        print ("\032[34;1m太高了, 你这只笨狗熊!  \033[0m")
     tries = tries +1
 if guess == secret:
-    print ("我的上帝！你居然发现了我的秘密!")
+    print ("\033[32;1m我的上帝！你居然发现了我的秘密!\033[0m")
 else:
-    print ("你唯一的机会已被你用完了！伙计!你就是只愚蠢的笨狗熊！")
-    print ("还是让我告诉你吧！这个数字是:%s" % secret)
+    print ("\033[32;1m你唯一的机会已被你用完了！伙计!你就是只愚蠢的笨狗熊！\033[0m")
+    print ("\033[31;1m还是让我告诉你吧！这个数字是:%s\033[0m" % secret)
